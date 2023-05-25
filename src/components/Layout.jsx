9f5e77381c6807716e2ee573bd7c25f29b381e0e
Imaniar Defend Mardotilah <mardotilahimaniar@gmail.com>
2023-05-25 21:13:54 +0700

    ToDoListApp

---
+++ src/components/Layout.jsx
@@ -0,0 +1,78 @@
+import { NavLink, Outlet } from "react-router-dom";
+import { actions } from '../features/todos/todosSlice';
+import { useDispatch, useSelector } from "react-redux";
+
+const Layout = () => {
+  const userInput = useSelector(state => state.todos.userInput);
+  const dispatch = useDispatch();
+
+  const handleCreateTodo = (e) => {
+    e.preventDefault();
+    dispatch(actions.createTodo());
+  }
+
+  const handleSetUserInput = (userInput) => {
+    dispatch(actions.setUserInput({ userInput }));
+  }
+
+  return (
+    <div className="container mx-auto mt-10 px-4 max-w-md">
+      <h1 className="text-center font-bold text-3xl mb-16">What's the plan for today?</h1>
+      <form className="flex gap-2 mb-12" onSubmit={handleCreateTodo}>
+        <input 
+          type="text"
+          value={userInput}
+          onChange={(e) => handleSetUserInput(e.target.value)}
+          placeholder="What to do"
+          className="p-2 w-full border-gray-300 border-solid border-2 rounded"
+        />
+        <input type="submit" className="btn bg-[#0EA5E9] text-white font-medium w-20" value="Add" />
+      </form>
+      <nav className="mt-4">
+        <ul className="flex gap-2 justify-center">
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+              isActive
+              ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#0EA5E9]"
+              : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500"
+              }
+            >
+              ALL
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/active"}
+              className={({ isActive }) =>
+              isActive
+              ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#0EA5E9]"
+              : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500"
+              }
+            >
+              ACTIVE
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/completed"}
+              className={({ isActive }) =>
+              isActive
+              ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#0EA5E9]"
+              : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500"
+              }
+            >
+              COMPLETED
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <section className="mt-4">
+        <Outlet />
+      </section>
+    </div>
+  );
+}
+
+export default Layout;
